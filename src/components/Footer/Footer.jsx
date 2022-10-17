89e49d7eec47602a61d2a17c93309fddbbc94527
WaiYanLin71 <scm.waiyanlinn@gmail.com>
2022-10-17 15:01:38 +0630

    add design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,114 +1,173 @@
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import SubscriptionsRoundedIcon from '@mui/icons-material/SubscriptionsRounded';
+import SendRoundedIcon from '@mui/icons-material/SendRounded';
 const Footer = () => {
-  return (
-    <div>
-      <div className="container lg:flex lg:justify-between lg:space-x-4 mt-10">
-        <div className="lg:w-1/4">
-        <h3 className="mb-8 text-xl">
-              </h3>
-        <div className="text-gray-500">Get the breathing space now, and we’ll extend your term at the other end year for go.
-              </div>
-        </div>
-          <div className="lg:w-1/4">
-              <h3 className="mb-8 text-xl">Book Category
-              </h3>
-              <div className="flex flex-col space-y-4">
-                  <div>
-                      <a className="hover:underline text-gray-500" href="">
-                          History
-                      </a>
-                  </div>
-                  <div>
-                      <a className="hover:underline text-gray-500" href="">
-                          Horror - Thriller
-                      </a>
-                  </div>
-                  <div>
-                      <a className="hover:underline text-gray-500" href="">
-                      Love Stories
-                      </a>
-                  </div>
-                  <div>
-                      <a className="hover:underline text-gray-500" href="">
-                      Science Fiction
-                      </a>
-                  </div>
-                  <div>
-                      <a className="hover:underline text-gray-500" href="">
-                      Business
-                      </a>
-                  </div>
-              </div>
-          </div>
+	return (
+		<div>
+			<div className='container lg:flex lg:justify-between lg:space-x-4 mt-20'>
+				<div className='lg:w-1/4'>
+					<h3 className='mb-8 text-xl'>
+                        <img src="./assets/image/xlogo2_footer.png.pagespeed.ic.wksTOD3AoG.webp" alt="" />
+                    </h3>
+					<div className='text-gray-500'>
+						Get the breathing space now, and we’ll extend your term
+						at the other end year for go.
+					</div>
+					<div className='mt-5 flex'>
+                        <div className="mr-3 cursor-pointer w-[40px] hover:bg-gray-200 h-[40px] flex items-center justify-center rounded-[50%] border-2 border-gray-200">
+                            <FacebookRoundedIcon/>
+                        </div>
+                        <div className="mr-3 cursor-pointer w-[40px] hover:bg-gray-200 h-[40px] flex items-center justify-center rounded-[50%] border-2 border-gray-200">
+                            <SubscriptionsRoundedIcon/>
+                        </div>
+                        <div className="mr-3 cursor-pointer w-[40px] hover:bg-gray-200 h-[40px] flex items-center justify-center rounded-[50%] border-2 border-gray-200">
+                            <SendRoundedIcon/>
+                        </div>
+                    </div>
+				</div>
+				<div className='lg:w-1/4'>
+					<h3 className='mb-8 text-xl'>Book Category</h3>
+					<div className='flex flex-col space-y-4'>
+						<div>
+							<a
+								className='hover:underline text-gray-500'
+								href=''
+							>
+								History
+							</a>
+						</div>
+						<div>
+							<a
+								className='hover:underline text-gray-500'
+								href=''
+							>
+								Horror - Thriller
+							</a>
+						</div>
+						<div>
+							<a
+								className='hover:underline text-gray-500'
+								href=''
+							>
+								Love Stories
+							</a>
+						</div>
+						<div>
+							<a
+								className='hover:underline text-gray-500'
+								href=''
+							>
+								Science Fiction
+							</a>
+						</div>
+						<div>
+							<a
+								className='hover:underline text-gray-500'
+								href=''
+							>
+								Business
+							</a>
+						</div>
+					</div>
+				</div>
 
-          <div className="lg:w-1/4 mt-[60px]">
-              <div className="flex flex-col space-y-4">
-                  <div>
-                      <a className="hover:underline text-gray-500" href="">
-                      Biography
-                      </a>
-                  </div>
-                  <div>
-                      <a className="hover:underline text-gray-500" href="">
-                      Astrology
-                      </a>
-                  </div>
-                  <div>
-                      <a className="hover:underline text-gray-500" href="">
-                      Digital Marketing
-                      </a>
-                  </div>
-                  <div>
-                      <a className="hover:underline text-gray-500" href="">
-                      Software Development
-                      </a>
-                  </div>
-                  <div>
-                      <a className="hover:underline text-gray-500" href="">
-                      Ecommerce
-                      </a>
-                  </div>
-              </div>
-          </div>
+				<div className='lg:w-1/4 mt-[60px]'>
+					<div className='flex flex-col space-y-4'>
+						<div>
+							<a
+								className='hover:underline text-gray-500'
+								href=''
+							>
+								Biography
+							</a>
+						</div>
+						<div>
+							<a
+								className='hover:underline text-gray-500'
+								href=''
+							>
+								Astrology
+							</a>
+						</div>
+						<div>
+							<a
+								className='hover:underline text-gray-500'
+								href=''
+							>
+								Digital Marketing
+							</a>
+						</div>
+						<div>
+							<a
+								className='hover:underline text-gray-500'
+								href=''
+							>
+								Software Development
+							</a>
+						</div>
+						<div>
+							<a
+								className='hover:underline text-gray-500'
+								href=''
+							>
+								Ecommerce
+							</a>
+						</div>
+					</div>
+				</div>
 
-          <div className="lg:w-1/4">
-              <h3 className="mb-8 text-xl">
-              Site Map
-              </h3>
-              <div className="flex flex-col space-y-4">
-                  <div>
-                      <a className="hover:underline text-gray-500" href="">
-                      Home
-                      </a>
-                  </div>
-                  <div>
-                      <a className="hover:underline text-gray-500" href="">
-                      About Us
-                      </a>
-                  </div>
-                  <div>
-                      <a className="hover:underline text-gray-500" href="">
-                      FAQs
-                      </a>
-                  </div>
-                  <div>
-                      <a className="hover:underline text-gray-500" href="">
-                      Blog
-                      </a>
-                  </div>
-                  <div>
-                      <a className="hover:underline text-gray-500" href="">
-                      Contact
-                      </a>
-                  </div>
-              </div>
-          </div>
-      </div>
+				<div className='lg:w-1/4'>
+					<h3 className='mb-8 text-xl'>Site Map</h3>
+					<div className='flex flex-col space-y-4'>
+						<div>
+							<a
+								className='hover:underline text-gray-500'
+								href=''
+							>
+								Home
+							</a>
+						</div>
+						<div>
+							<a
+								className='hover:underline text-gray-500'
+								href=''
+							>
+								About Us
+							</a>
+						</div>
+						<div>
+							<a
+								className='hover:underline text-gray-500'
+								href=''
+							>
+								FAQs
+							</a>
+						</div>
+						<div>
+							<a
+								className='hover:underline text-gray-500'
+								href=''
+							>
+								Blog
+							</a>
+						</div>
+						<div>
+							<a
+								className='hover:underline text-gray-500'
+								href=''
+							>
+								Contact
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
 
-      <div className="container mt-6 border-t border-light bg-primary/50 py-6 text-center text-sm">
-      Copyright ©2022 All rights reserved | wyl@gmail.com
-      </div>
-    </div>
-  )
-}
+			<div className='container mt-6 border-t border-light bg-primary/50 py-6 text-center text-sm'>
+				Styled with tailwincss and developed with <a href='' className='mx-2 text-red-600'>Hsu</a>|<a href='' className='mx-2 text-red-600'>Kyi</a>|<a href="" className='mx-2 text-red-600'>Wyl</a>
+			</div>
+		</div>
+	);
+};
 
-export default Footer
+export default Footer;
--- src/components/NewLetter/NewLetter.jsx
+++ src/components/NewLetter/NewLetter.jsx
@@ -1,20 +1,39 @@
 const NewLetter = () => {
-  return (
-    <div className="container place-content-center p-20 text-center">
-      <h1 className="text-4xl font-serif">Join Newsletter</h1>
-      <p className="px-56 py-7">Lorem started its journey with cast iron (CI) products in 1980. The initial main objective was to ensure pure water and affordable irrigation.</p>
-      <div className="flex flex-col justify-center items-center">
-        <form className="w-full max-w-sm">
-          <div className="flex items-center py-2">
-            <input className="rounded-3xl bg-transparent w-full text-gray-700 mr-3 py-3 px-2 focus:outline-none" type="text" placeholder="Enter Your Email" aria-label="Full name"/>
-            <button className="rounded-full flex-shrink-0 bg-red-500 hover:bg-red-700 border-red-500 hover:border-red-700 text-sm border-4 text-white py-2 px-3" type="button">
-              Subscribe
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  )
-}
+    const image = './assets/image/section-bg1.jpg.webp'
+    const bgImage = {
+        backgroundImage : `url(${image})`,
+        backgroundRepeat :'no-repeat',
+    }
+	return (
+		<div className='container place-content-center text-center mt-10'>
+            <div  style={bgImage} className="p-20 text-white">
+            <h1 className='text-4xl font-serif'>Join Newsletter</h1>
+			<p className='px-56 py-7'>
+				Lorem started its journey with cast iron (CI) products in 1980.
+				The initial main objective was to ensure pure water and
+				affordable irrigation.
+			</p>
+			<div className='flex flex-col justify-center items-center'>
+				<form className='w-full max-w-sm'>
+					<div className='flex items-center py-2'>
+						<input
+							className='rounded-3xl bg-white pl-4 w-full text-gray-700 mr-3 py-3 px-2 focus:outline-none'
+							type='text'
+							placeholder='Enter Your Email'
+							aria-label='Full name'
+						/>
+						<button
+							className='rounded-full flex-shrink-0 bg-red-500 hover:bg-red-700 border-red-500 hover:border-red-700 text-sm border-4 text-white py-2 px-3'
+							type='button'
+						>
+							Subscribe
+						</button>
+					</div>
+				</form>
+			</div>
+            </div>
+		</div>
+	);
+};
 
-export default NewLetter
+export default NewLetter;
