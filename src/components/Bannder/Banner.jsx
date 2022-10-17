7a34b214459d2dc8d8111e9ff289f270b24e0b6c
ChanHeinKyaw <scm.chanheinkyaw@gmail.com>
2022-10-17 16:17:19 +0630

    Bander

--- src/components/Bannder/Banner.jsx
+++ src/components/Bannder/Banner.jsx
@@ -1,8 +1,20 @@
 const Banner = () => {
+  const bander = {
+		backgroundImage: "url(./assets/image/bander.webp)",
+		backgroundRepeat: "no-repeat",
+    backgroundSize : "cover",
+	};
   return (
-    <div>
-      
-    </div>
+      <div className="container place-content-center text-center mt-10 items-center">
+        <div style={bander} className="p-40 text-white">
+            <button className="rounded-3xl bg-white py-2 px-5 text-gray-400 text-sm mb-6">Science Fiction</button>
+            <h1 className="text-6xl font-serif leading-[70px]">
+              The History 
+              <br/>  
+              of Phipino</h1>
+            <button className="rounded-3xl bg-red-600 py-3 px-10 mt-8">Browse Store</button>
+        </div>
+      </div>
   )
 }
 
