4705e89d6b6b0f5acc3d5e5a04a1be84b3a7d9fd
HsuMonLynn <scm.hsumonlynn@gmail.com>
2022-10-17 14:36:02 +0630

    add banner

--- src/components/NewLetter/NewLetter.jsx
+++ src/components/NewLetter/NewLetter.jsx
@@ -1,7 +1,18 @@
 const NewLetter = () => {
   return (
-    <div>
-      
+    <div className="container place-content-center p-20 text-center">
+      <h1 className="text-4xl font-serif">Join Newsletter</h1>
+      <p className="px-56 py-7">Lorem started its journey with cast iron (CI) products in 1980. The initial main objective was to ensure pure water and affordable irrigation.</p>
+      <div className="flex flex-col justify-center items-center">
+        <form className="w-full max-w-sm">
+          <div className="flex items-center py-2">
+            <input className="rounded-3xl bg-transparent w-full text-gray-700 mr-3 py-3 px-2 focus:outline-none" type="text" placeholder="Enter Your Email" aria-label="Full name"/>
+            <button className="rounded-full flex-shrink-0 bg-red-500 hover:bg-red-700 border-red-500 hover:border-red-700 text-sm border-4 text-white py-2 px-3" type="button">
+              Subscribe
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
