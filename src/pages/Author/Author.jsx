030b060d1216715c24792ae22b934f7d4852a82b
ChanHeinKyaw <scm.chanheinkyaw@gmail.com>
2022-11-24 16:39:35 +0630

    author-page

--- src/pages/Author/Author.jsx
+++ src/pages/Author/Author.jsx
@@ -1,23 +1,110 @@
 const Author = () => {
 	return (
-		<>
-			<div className='container'>
-				<h2 className='text-3xl font-bold text-center mt-10'>
+		<div>
+			<div className='container bg-pink-soft border border-white p-10 mt-10 rounded-lg'>
+				<h2 className='text-3xl font-bold text-left'>
 					Authors
 				</h2>
-				<div className='grid grid-cols-4 gap-5 mt-5'>
-					<div class='max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'>
+				<div className='grid grid-cols-4 gap-20 mt-5'>
+					<div class="w-full">
 						<a href='#'>
 							<img
-								class='rounded-t-lg'
+								class='rounded-[10%]'
 								src='https://flowbite.com/docs/images/blog/image-1.jpg'
 								alt=''
 							/>
 						</a>
+						<div class="flex justify-center pt-3">
+						<span class="text-xl text-gray-500">Chan Hein Kyaw</span>
+						</div>
+					</div>
+					<div class="w-full">
+						<a href='#'>
+							<img
+								class='rounded-[10%]'
+								src='https://flowbite.com/docs/images/blog/image-1.jpg'
+								alt=''
+							/>
+						</a>
+						<div class="flex justify-center pt-3">
+						<span class="text-xl text-gray-500">Chan Hein Kyaw</span>
+						</div>
+					</div>
+					<div class="w-full">
+						<a href='#'>
+							<img
+								class='rounded-[10%]'
+								src='https://flowbite.com/docs/images/blog/image-1.jpg'
+								alt=''
+							/>
+						</a>
+						<div class="flex justify-center pt-3">
+						<span class="text-xl text-gray-500">Chan Hein Kyaw</span>
+						</div>
+					</div>
+					<div class="w-full">
+						<a href='#'>
+							<img
+								class='rounded-[10%]'
+								src='https://flowbite.com/docs/images/blog/image-1.jpg'
+								alt=''
+							/>
+						</a>
+						<div class="flex justify-center pt-3">
+						<span class="text-xl text-gray-500">Chan Hein Kyaw</span>
+						</div>
+					</div>
+					<div class="w-full">
+						<a href='#'>
+							<img
+								class='rounded-[10%]'
+								src='https://flowbite.com/docs/images/blog/image-1.jpg'
+								alt=''
+							/>
+						</a>
+						<div class="flex justify-center pt-3">
+						<span class="text-xl text-gray-500">Chan Hein Kyaw</span>
+						</div>
+					</div>
+					<div class="w-full">
+						<a href='#'>
+							<img
+								class='rounded-[10%]'
+								src='https://flowbite.com/docs/images/blog/image-1.jpg'
+								alt=''
+							/>
+						</a>
+						<div class="flex justify-center pt-3">
+						<span class="text-xl text-gray-500">Chan Hein Kyaw</span>
+						</div>
+					</div>
+					<div class="w-full">
+						<a href='#'>
+							<img
+								class='rounded-[10%]'
+								src='https://flowbite.com/docs/images/blog/image-1.jpg'
+								alt=''
+							/>
+						</a>
+						<div class="flex justify-center pt-3">
+						<span class="text-xl text-gray-500">Chan Hein Kyaw</span>
+						</div>
+					</div>
+					<div class="w-full">
+						<a href='#'>
+							<img
+								class='rounded-[10%]'
+								src='https://flowbite.com/docs/images/blog/image-1.jpg'
+								alt=''
+							/>
+						</a>
+						<div class="flex justify-center pt-3">
+						<span class="text-xl text-gray-500">Chan Hein Kyaw</span>
+						</div>
 					</div>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 };
 
